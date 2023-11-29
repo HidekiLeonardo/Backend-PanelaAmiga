be0f10120a7b6513f3b29b4995edf3b7f2b8640e
pedropedroo <142456675+pedropedroo@users.noreply.github.com>
2023-11-29 19:37:48 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,3 @@
 <?php
-$servidor = '127.0.0.1';
-$nomeUsuario = 'root';
-$senha = '';
-$nomeBanco = 'bdpanelaamiga';
 
-try {
-    $conexao = new PDO("mysql:host=$servidor;dbname=$nomeBanco", $nomeUsuario, $senha);
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-}
 ?>
