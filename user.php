b9ac83f02f593c73f2c4caf87c6af4eb39c1090d
pedropedroo <142456675+pedropedroo@users.noreply.github.com>
2023-11-29 19:38:23 -0300

    Update user.php

--- user.php
+++ user.php
@@ -1,5 +1,15 @@
 <?php
-include_once('conexao.php');
+$servidor = '127.0.0.1';
+$nomeUsuario = 'root';
+$senha = '';
+$nomeBanco = 'bdpanelaamiga';
+
+try {
+    $conexao = new PDO("mysql:host=$servidor;dbname=$nomeBanco", $nomeUsuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = isset($_POST['E-mail']) ? $_POST['E-mail'] : '';
